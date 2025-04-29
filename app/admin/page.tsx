beb37207d14ec63456ee974956b72a1030ee0885
Jack Greenwald <72763093+JGreenwald538@users.noreply.github.com>
2025-04-29 10:23:51 -0400

    Added data validation

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,11 +1,32 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import PrintifyProduct from "../utils/PrintifyProduct";
 
 import { checkPassword } from "../utils/CheckPassword";
 import { DataRowTshirt } from "../utils/DataRowTshirt";
 import { DataRowPoster } from "../utils/DataRowPoster";
+const tshirtHeader = [
+	"ID",
+	"Product Name",
+	"Small Price",
+	"Medium Price",
+	"Large Price",
+	"XL Price",
+	"2XL Price",
+	"3XL Price",
+];
+
+const posterHeader = [
+	"ID",
+	"Product Name",
+	'"11""x14"" Price"',
+	'"12""x16"" Price"',
+	'"16""x20"" Price"',
+	'"20""x24"" Price"',
+	'"18""x24"" Price"',
+	'"24""x32"" Price"',
+];
 
 export default function AdminPage() {
 	const [tshirts, setTshirts] = useState<PrintifyProduct[]>([]);
@@ -17,6 +38,8 @@ export default function AdminPage() {
 	}, []);
 	const [tshirtsCSV, setTshirtsCSV] = useState<DataRowTshirt[]>([]);
 	const [postersCSV, setPostersCSV] = useState<DataRowPoster[]>([]);
+	const tshirtInputRef = useRef<HTMLInputElement | null>(null);
+	const posterInputRef = useRef<HTMLInputElement | null>(null);
 
 	const handlePasswordSubmit = (first: boolean) => {
 		checkPassword(password)
@@ -109,7 +132,7 @@ export default function AdminPage() {
 			{isAuthenticated ? (
 				<div className="flex flex-col items-center justify-center h-screen">
 					<h1 className="text-4xl font-bold mb-4">Admin Page</h1>
-					<div className="flex flex-col items-center space-y-5">
+					<div className="flex flex-col items-center space-y-10">
 						<button
 							className="bg-blue-300 rounded px-2 py-1 w-fit "
 							onClick={async () => {
@@ -147,6 +170,7 @@ export default function AdminPage() {
 						<input
 							type="file"
 							accept=".csv"
+							ref={tshirtInputRef}
 							onChange={async (file) => {
 								const rows: DataRowTshirt[] = [];
 								if (file.target.files) {
@@ -154,32 +178,44 @@ export default function AdminPage() {
 									const tableSplit = tableText
 										.split("\r\n")
 										.map((row: string) => row.split(","));
-									for (let i = 1; i < tableSplit.length; i++) {
-										const row: DataRowTshirt = {
-											ID: tableSplit[i][0],
-											"Product Name": tableSplit[i][1],
-											"Small Price": parseFloat(
-												tableSplit[i][2].replace(/[$]/g, "")
-											),
-											"Medium Price": parseFloat(
-												tableSplit[i][3].replace(/[$]/g, "")
-											),
-											"Large Price": parseFloat(
-												tableSplit[i][4].replace(/[$]/g, "")
-											),
-											"XL Price": parseFloat(
-												tableSplit[i][5].replace(/[$]/g, "")
-											),
-											"2XL Price": parseFloat(
-												tableSplit[i][6].replace(/[$]/g, "")
-											),
-											"3XL Price": parseFloat(
-												tableSplit[i][7].replace(/[$]/g, "")
-											),
-										};
-										rows.push(row);
+									if (
+										!tableSplit[0].every(
+											(value: string, index: number) =>
+												value === tshirtHeader[index]
+										)
+									) {
+										alert("Invalid Input File");
+										if (tshirtInputRef.current) {
+											tshirtInputRef.current.value = "";
+										}
+									} else {
+										for (let i = 1; i < tableSplit.length; i++) {
+											const row: DataRowTshirt = {
+												ID: tableSplit[i][0],
+												"Product Name": tableSplit[i][1],
+												"Small Price": parseFloat(
+													tableSplit[i][2].replace(/[$]/g, "")
+												),
+												"Medium Price": parseFloat(
+													tableSplit[i][3].replace(/[$]/g, "")
+												),
+												"Large Price": parseFloat(
+													tableSplit[i][4].replace(/[$]/g, "")
+												),
+												"XL Price": parseFloat(
+													tableSplit[i][5].replace(/[$]/g, "")
+												),
+												"2XL Price": parseFloat(
+													tableSplit[i][6].replace(/[$]/g, "")
+												),
+												"3XL Price": parseFloat(
+													tableSplit[i][7].replace(/[$]/g, "")
+												),
+											};
+											rows.push(row);
+										}
+										setTshirtsCSV(rows);
 									}
-									setTshirtsCSV(rows);
 								}
 							}}
 						/>
@@ -192,6 +228,7 @@ export default function AdminPage() {
 						<input
 							type="file"
 							accept=".csv"
+							ref={posterInputRef}
 							onChange={async (file) => {
 								const rows: DataRowPoster[] = [];
 								if (file.target.files) {
@@ -199,32 +236,44 @@ export default function AdminPage() {
 									const tableSplit = tableText
 										.split("\r\n")
 										.map((row: string) => row.split(","));
-									for (let i = 1; i < tableSplit.length; i++) {
-										const row: DataRowPoster = {
-											ID: tableSplit[i][0],
-											"Product Name": tableSplit[i][1],
-											'11"x14" Price': parseFloat(
-												tableSplit[i][2].replace(/[$]/g, "")
-											),
-											'12"x16" Price': parseFloat(
-												tableSplit[i][3].replace(/[$]/g, "")
-											),
-											'16"x20" Price': parseFloat(
-												tableSplit[i][4].replace(/[$]/g, "")
-											),
-											'20"x24" Price': parseFloat(
-												tableSplit[i][5].replace(/[$]/g, "")
-											),
-											'18"x24" Price': parseFloat(
-												tableSplit[i][6].replace(/[$]/g, "")
-											),
-											'24"x32" Price': parseFloat(
-												tableSplit[i][7].replace(/[$]/g, "")
-											),
-										};
-										rows.push(row);
+									if (
+										!tableSplit[0].every(
+											(value: string, index: number) =>
+												value === posterHeader[index]
+										)
+									) {
+										alert("Invalid Input File");
+										if (posterInputRef.current) {
+											posterInputRef.current.value = "";
+										}
+									} else {
+										for (let i = 1; i < tableSplit.length; i++) {
+											const row: DataRowPoster = {
+												ID: tableSplit[i][0],
+												"Product Name": tableSplit[i][1],
+												'11"x14" Price': parseFloat(
+													tableSplit[i][2].replace(/[$]/g, "")
+												),
+												'12"x16" Price': parseFloat(
+													tableSplit[i][3].replace(/[$]/g, "")
+												),
+												'16"x20" Price': parseFloat(
+													tableSplit[i][4].replace(/[$]/g, "")
+												),
+												'20"x24" Price': parseFloat(
+													tableSplit[i][5].replace(/[$]/g, "")
+												),
+												'18"x24" Price': parseFloat(
+													tableSplit[i][6].replace(/[$]/g, "")
+												),
+												'24"x32" Price': parseFloat(
+													tableSplit[i][7].replace(/[$]/g, "")
+												),
+											};
+											rows.push(row);
+										}
+										setPostersCSV(rows);
 									}
-									setPostersCSV(rows);
 								}
 							}}
 						/>
