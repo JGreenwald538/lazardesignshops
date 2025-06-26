00848725eb15b1a0731ef464625117103ea548f3
Jack Greenwald <72763093+JGreenwald538@users.noreply.github.com>
2025-06-26 10:08:56 -0400

    Updated top bar mobile

--- app/components/TopBar.tsx
+++ app/components/TopBar.tsx
@@ -27,27 +27,33 @@ export default function TopBar() {
 				/>
 			</Link>
 			<div className="lg:w-full lg:px-0 px-5 rounded flex lg:flex-row flex-col py-5 items-center lg:gap-y-0 gap-y-5">
-				<div className="flex-1 flex justify-self-start lg:gap-x-5 lg:ml-5 lg:flex-row flex-col lg:gap-y-0 gap-y-3">
+				<div className="flex-1 lg:flex justify-self-start gap-x-5 lg:ml-5 lg:flex-row lg:gap-y-0 gap-y-3 grid grid-cols-2">
 					<a
-						className="text-xl bg-[#e35050] text-white rounded px-3 py-1 whitespace-pre"
+						className="text-xl bg-[#e35050] text-white rounded px-3 py-1 whitespace-pre text-center"
 						href={"/?f=posters"}
 					>
 						{"Posters"}
 					</a>
 					<a
-						className="text-xl bg-[#e35050] text-white rounded px-3 py-1"
+						className="text-xl bg-[#e35050] text-white rounded px-3 py-1 text-center"
 						href={"/?f=tshirts"}
 					>
 						T-Shirts
 					</a>
-					<a className="text-xl bg-[#e35050] text-white rounded px-4 py-1 whitespace-pre">
+					<a className="text-xl bg-[#e35050] text-white rounded px-4 py-1 whitespace-pre text-center">
 						{" Other "}
 					</a>
+					<Link
+						className="text-xl bg-[#e35050] text-white rounded px-5 py-1 text-center lg:hidden"
+						href={"/contact"}
+					>
+						Contact
+					</Link>
 				</div>
 				{/* <input placeholder="Search for products" type="text" name="text" className="flex-1 p-2 rounded-md border-[1.5px] border-gray-200 mr-5 active:border-gray-400 "></input> */}
 				<div className="flex lg:space-x-4 lg:mr-5 justify-self-end h-fit lg:flex-row flex-col items-center lg:gap-y-0 gap-y-3">
 					<Link
-						className="text-xl bg-[#e35050] text-white rounded px-5 py-1 text-center"
+						className="text-xl bg-[#e35050] text-white rounded px-5 py-1 text-center lg:block hidden"
 						href={"/contact"}
 					>
 						Contact
@@ -59,7 +65,11 @@ export default function TopBar() {
 						Custom Order
 					</Link>
 					<Link href={"/checkout/review"}>
-						{cartNumber !== 0 && <div className="absolute rounded-full bg-white w-5 h-5 text-center translate-x-6 -translate-y-2 text-sm">{cartNumber}</div>}
+						{cartNumber !== 0 && (
+							<div className="absolute rounded-full bg-white w-5 h-5 text-center translate-x-6 -translate-y-2 text-sm">
+								{cartNumber}
+							</div>
+						)}
 						<FaShoppingCart className="text-4xl text-black py-1" />
 					</Link>
 				</div>
